7c1a59e1b2000da8c3e7ce5411c9bff1c9ab5bc0
Nacho Ortega <iortega@atsistemas.com>
2018-11-09 09:31:46 +0100

    3:16 default prop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class Title extends Component {
+  render () {
+    return (
+      <h1>{this.props.text}</h1>
+    )
+  }
+}
+
+Title.defaultProps = {
+  text: 'Título por defecto'
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Title text='otro'/>
       </div>
     );
   }
