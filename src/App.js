eeb48cc095d4cf15a04b23e12e34d1160037a2b3
Nacho <iortega@atsistemas.com>
2019-02-01 09:28:19 +0100

    6:34 Formularios en react

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+import Forms from './sections/forms';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      mouseX: 0,
-      mouseY: 0
-    };
-    //this.handleMouseMove = this.handleMouseMove.bind(this);
-
-  }
-
-  handleMouseMove = (e) => {
-    const { clientX, clientY } = e;
-    this.setState({ mouseX: clientX, mouseY: clientY });
-  }
-
-  handleCLick(e) {
-    console.log(e);
-    alert("hola");
-  }
 
   render() {
     return (
       <div className="App">
-        <h4>Eventos</h4>
-        <button onClick={this.handleCLick}>Hola</button>
-        <div
-          onMouseMove={this.handleMouseMove}
-          style={{ border: "1px solid #000", marginTop: 10, padding: 10 }}
-        >
-          <p>
-            {this.state.mouseX}, {this.state.mouseY}
-          </p>
-        </div>
+        <Forms />       
       </div>
     );
   }
