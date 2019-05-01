dd9e315c8640ae150ffb9ca760ede6dec2c3d2b5
Nacho <iortega@atsistemas.com>
2019-05-01 17:20:38 +0200

    6:40 children prop

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import React, { Component } from "react";
 import "./App.css";
 import Forms from './sections/forms';
 
+
+class Box extends Component {
+  render() {
+    return (
+      <div style= {{border: '1px solid #000', margin: 5, padding: 5}}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <Forms />       
+        <h4>Children Props</h4> 
+        <Box>Children 1</Box>
+        <Box>Children 2</Box> 
       </div>
     );
   }
