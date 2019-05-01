3c4910cc1aee1886e02ccc0941f558a07ee9ead8
Nacho <iortega@atsistemas.com>
2019-05-01 19:32:05 +0200

    8:45 El Constructor

--- src/App.js
+++ src/App.js
@@ -1,45 +1,28 @@
 import React, { Component } from "react";
 import "./App.css";
-import PropTypes from 'prop-types';
 
-
-class Article extends Component {
-  static propTypes = {
-    author: PropTypes.string.isRequired
-  }
-  constructor(props) {
-    super(props)
-    if(typeof props.author === 'undefined'){
-      console.warn("prop required");
-    }
-  }
-  render() {
-    const {author, children, date, title} = this.props
-    return (
-      <section>
-        <h2>{title}</h2>
-        {author && <p><em>Escrito por {author}</em></p>}
-        <time>{date}</time>
-        <article>
-          {children}
-        </article>
-      </section>
-    )
-  }
-}
 
 class App extends Component {
 
+  constructor(props) {
+    console.log("Constructor");
+    super(props);
+    this.state = {mensajeInicial: 'Inicial'}
+  }
+
+  handleClick = () => {
+    this.setState({mensajeInicial: ' cambiado'})
+  }
+
   render() {
+    console.log("Render");
     return (
       <div className="App">
-        <h4>Children Props</h4> 
-        <Article
-          author='Nacho'
-          date={new Date().toLocaleDateString()}
-          title='Artículo sobre children'>
-          <p>El contenido que envolvemos dentro del componente será enviado al children. <strong>Y mantiene etiquetas</strong></p>
-        </Article>
+        <h4>Ciclo de montaje: constructor</h4>
+        {this.state.mensajeInicial}
+        <button onClick={this.handleClick}>
+          Cambiar Mensaje
+        </button>
       </div>
     );
   }
