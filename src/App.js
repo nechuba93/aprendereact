8f0f5ebac9bdbb9407d103efda64af5ea69cb9af
Nacho <iortega@atsistemas.com>
2019-05-04 20:33:21 +0200

    8:49 Fetch API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
-import EjemploDeComponentDidMount from "./sections/life-cicle/componentDidMount";
+import FetchExample from "./sections/fetch-example";
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <EjemploDeComponentDidMount />
+        <FetchExample />
       </div>
     );
   }
